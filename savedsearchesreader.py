e7c4e445b81f10c7f3c35df90799ccbc8487eaa2
cwobuzz <67645073+cwobuzz@users.noreply.github.com>
2022-09-13 14:48:15 -0600

    Added exclude rule name file, caused a number of changes

--- savedsearchesreader.py
+++ savedsearchesreader.py
@@ -14,12 +14,15 @@ default_savedsearchesconf = os.path.abspath("./dist/ESCU_Alerts/default/savedsea
 local_savedsearches = os.path.abspath("./dist/ESCU_Alerts/local/savedsearches.conf")
 # searches through rule name for words you don't want in use in your enviroment like GSuite or Amazon
 filter_list_for_rule_name = ["Amazon", "Gsuite", "GSuite ", "AWS", " aws ", "EC2", "GCP", " gcp ", "Okta"]
-# Remove any providing technologies only put over all name like Amazon, not Amazon Web Services
+# Remove any providing technologies only put one term like Amazon, not "Amazon Web Services"
 input_providing_technologies = "Amazon"
 # Enable Rule sets the action.escu, action.escu.enabled, action.risk, action.correlationsearch.enabled, enableSched all to 1
 enable_rule = True
+# How much to change risk by. 1 keeps it default. Divides risk by input.
+risk_division_amount = 4
+# path to a text file called rules_to_exclude that will exclude any rule by name in it. Please have every rule name match this format: "ESCU APP - Recon Using WMI Class - Rule"
+rules_to_exclude_file = os.path.abspath("rules_to_exclude.txt")
 
-#filter_list_for_rule_name = filter_list_for_rule_name.split()
 # set up reg expresstion for parsing conf file
 rule = re.compile(r'(^\[.+)')
 providing_technologies = re.compile(r'(action\.escu\.providing_technologies).+')
@@ -32,8 +35,10 @@ correlationsearch_annotations = re.compile(r'(action\.correlationsearch\.annotat
 correlationsearch_enabled = re.compile(r'(action\.correlationsearch\.enabled).+')
 correlationsearch_label = re.compile(r'(action\.correlationsearch\.label).+')
 action_escu = re.compile(r'(action\.escu =).+')
+action_noteable = re.compile(r'(action\.notable =).+')
 is_enabled = re.compile(r'(action\.escu\.enabled).+')
-risk = re.compile(r'(action\.risk).+')
+risk = re.compile(r'(action\.risk =).+')
+action_risk_param_risk = re.compile(r'(action\.risk\.param\.\_risk =).+')
 dispatch = re.compile(r'(dispatch\.).+')
 schedule_window = re.compile(r'(schedule_window).+')
 alert_digest_mode = re.compile(r'(alert\.digest_mode).+')
@@ -45,6 +50,7 @@ relation = re.compile(r'(relation).+')
 quantity = re.compile(r'(quantity).+')
 realtime_schedule = re.compile(r'(realtime_schedule).+')
 is_visible = re.compile(r'(is_visible).+')
+escu_app = re.compile(r'(\[ESCU APP -).+')
 
 #Dictionary for data
 Dict = {}
@@ -69,9 +75,17 @@ def parse_local_savedsearches(local_savedsearches):
         for local_ruleset in local_readfile:
             for line in local_ruleset.splitlines():
                 if rule.match(line):
-                    Dict[line] = {}
-                    Dict[line][line] = line
-                    dic_rule = line
+                    
+                    if escu_app.match(line):
+                        Dict[line] = {}
+                        Dict[line][line] = line
+                        dic_rule = line
+                    # Need to change rule name or you get a bunch of errors saying the Name is used in other apps    
+                    else:    
+                        dic_rule = "[ESCU APP -"+ line.split("ESCU -")[1]
+                        Dict[dic_rule] = {}
+                        Dict[dic_rule][dic_rule] = dic_rule
+                        
                     continue
                     #Dict[line][line] = line   
                 if is_enabled.match(line):
@@ -89,6 +103,9 @@ def parse_local_savedsearches(local_savedsearches):
                 if correlationsearch_label.match(line):
                     Dict[dic_rule]['correlationsearch_label'] = {line}
                     continue
+                if action_noteable.match(line):
+                    Dict[dic_rule]['action_notable'] = {"action.notable = 0"}
+                    continue
                 # for keeping track of modification date of rule
                 if modification_date.match(line):
                     Dict[dic_rule]['modification'] = {line}
@@ -98,6 +115,9 @@ def parse_local_savedsearches(local_savedsearches):
                     continue
                 if risk.match(line):
                     Dict[dic_rule]['risk'] = {line}
+                    continue
+                if action_risk_param_risk.match(line):
+                    Dict[dic_rule]['riskamount'] = {(line)}
                     continue
                 if cron_schedule.match(line):
                     Dict[dic_rule]['cron_schedule'] = {line}
@@ -141,10 +161,13 @@ def parse_local_savedsearches(local_savedsearches):
                 else:
                     word = line.split(" = ")[0]
                     Dict[dic_rule][word] = {line}
-        #localsavedsearches.close()
+        
     return Dict
     
 Dict = parse_local_savedsearches(local_savedsearches)        
+with open (rules_to_exclude_file, "r") as file_exclude:
+    rules_to_exclude = file_exclude.read()
+    file_exclude.close()
 
 def parse_file(default_savedsearchesconf):
 
@@ -167,19 +190,19 @@ def parse_file(default_savedsearchesconf):
                 if rule.match(line):
                     
                     if not any(value in line for value in filter_list_for_rule_name):    
-                            keep_data_rule = True
-                            
-                    for key in Dict:
-                        if line == str(key):
-                            dic_rule = line
-                            localrule = True
-                            break
-                                                        
-                    if localrule != True: 
-                        if keep_data_rule == True:
-                            Dict[line] = {}    
-                            Dict[line][line] = line
-                            dic_rule = line       
+                        keep_data_rule = True
+                        dic_rule = "[ESCU APP -"+ line.split("ESCU -")[1]
+                        for x in Dict:
+                            if x == dic_rule:
+                                localrule = True
+                    if localrule == False:          
+                        Dict[dic_rule] = {}
+                        Dict[dic_rule][dic_rule] = dic_rule
+                                                                        
+                    for exclude in rules_to_exclude.splitlines():
+                        if exclude == dic_rule.strip("[]"):
+                            keep_data_rule = False
+                                
 
                 # makes sure the rules are enabled
                 if disabled.match(line):
@@ -206,6 +229,13 @@ def parse_file(default_savedsearchesconf):
                     if keep_data_rule == True:
                         if enable_rule == True:
                             Dict[dic_rule]['enableSched'] = "enableSched = 1"
+
+                # changes risk amount
+                if action_risk_param_risk.match(line):
+                    if keep_data_rule == True:
+                        if enable_rule == True:
+                            if localrule == False:
+                                Dict[dic_rule]['riskamount'] = re.sub(r'\"risk_score\": (\d+)', lambda r: '\"risk_score\": ' + str(round(int(r.group(1))/risk_division_amount)) ,line)
                 # for fidelity matching on rule search
                 if confidence.match(line):
                     if keep_data_rule == True:
@@ -236,7 +266,8 @@ def parse_file(default_savedsearchesconf):
                 # random con so every hour at some random time it will run that way it's not all 0
                 if cron_schedule.match(line):
                     if keep_data_rule == True:
-                        Dict[dic_rule]['cron_schedule'] = 'cron_schedule = ' + str(random.randrange(1,59)) + ' * * * *'
+                        if localrule == False:
+                            Dict[dic_rule]['cron_schedule'] = 'cron_schedule = ' + str(random.randrange(1,59)) + ' * * * *'
 
                 if search_type.match(line):
                     if line != "action.escu.search_type = detection":
@@ -273,6 +304,7 @@ def parse_file(default_savedsearchesconf):
     return Dict
 data = parse_file(default_savedsearchesconf)
 localsavedsearches = open(local_savedsearches, "w")
+
 def iterate_dict(data):
     for key, value in data.items():
         # print(key)
@@ -293,4 +325,3 @@ def iterate_dict(data):
 iterate_dict(data)  
 
 localsavedsearches.close()
-
